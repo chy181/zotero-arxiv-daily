386ed3840963f91099c351c1ad8401f1de2425f2
hanyinch <90826776+chy181@users.noreply.github.com>
2025-10-03 15:33:23 +0800

    Fix email sending function argument order

--- main.py
+++ main.py
@@ -183,6 +183,6 @@ if __name__ == '__main__':
 
     html = render_email(papers)
     logger.info("Sending email...")
-    send_email(args.sender, json.dupms(['hycheng@stu.ecnu.edu.cn','xjwu@stu.ecnu.edu.cn','xfqiu@stu.ecnu.edu.cn','1463250073@qq.com')], args.sender_password, args.smtp_server, args.smtp_port, html)
+    send_email(args.sender, json.dupms(['hycheng@stu.ecnu.edu.cn','xjwu@stu.ecnu.edu.cn','xfqiu@stu.ecnu.edu.cn','1463250073@qq.com']), args.sender_password, args.smtp_server, args.smtp_port, html)
     logger.success("Email sent successfully! If you don't receive the email, please check the configuration and the junk box.")
 
